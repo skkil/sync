5f1661dafb8569de473d836a27dc9394c71d287f
Hyoungjoo Jin <hyoungjoo.j@gmail.com>
2026-01-22 15:07:41 +0900

    feat: 웹 클라이언트 로그인 및 회원가입 기본 ui 추가 (#7)

---
+++ apps/web/src/app/auth/login/_components/LoginForm.tsx
@@ -0,0 +1,112 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useTranslations } from 'next-intl';
+import Link from 'next/link';
+import { Controller, useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+import * as z from 'zod';
+
+import { Button } from '@/components/ui/button';
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from '@/components/ui/field';
+import { Input } from '@/components/ui/input';
+
+export default function LoginForm() {
+  const t = useTranslations('pages.login.form');
+
+  const LoginFormSchema = z.object({
+    email: z
+      .email({ message: t('errors.invalid_credentials') })
+      .nonoptional({ message: t('errors.required_email') }),
+    password: z.string().nonempty({ message: t('errors.required_password') }),
+  });
+
+  type LoginFormValues = z.infer<typeof LoginFormSchema>;
+
+  const form = useForm<LoginFormValues>({
+    resolver: zodResolver(LoginFormSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onFormSubmit = async (values: LoginFormValues) => {
+    toast.info('Form submitted' + JSON.stringify(values));
+  };
+
+  return (
+    <div>
+      <form onSubmit={form.handleSubmit(onFormSubmit)}>
+        <FieldGroup>
+          <Controller
+            name="email"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <div className="flex items-center justify-between">
+                  <FieldLabel htmlFor="login-form-email">
+                    {t('email.label')}
+                  </FieldLabel>
+
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </div>
+
+                <Input
+                  {...field}
+                  id="login-form-email"
+                  aria-invalid={fieldState.invalid}
+                  placeholder={t('email.placeholder')}
+                  autoComplete="email"
+                />
+              </Field>
+            )}
+          />
+          <Controller
+            name="password"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <div className="flex items-center justify-between">
+                  <FieldLabel htmlFor="login-form-password">
+                    {t('password.label')}
+                  </FieldLabel>
+
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </div>
+
+                <Input
+                  {...field}
+                  id="login-form-password"
+                  type="password"
+                  aria-invalid={fieldState.invalid}
+                  placeholder={t('password.placeholder')}
+                  autoComplete="current-password"
+                />
+              </Field>
+            )}
+          />
+
+          <div>
+            <Button className="w-full" type="submit">
+              {t('submit.label')}
+            </Button>
+
+            <Button className="w-full" variant="link">
+              <Link href="/auth/register">{t('links.register.label')}</Link>
+            </Button>
+          </div>
+        </FieldGroup>
+      </form>
+    </div>
+  );
+}
