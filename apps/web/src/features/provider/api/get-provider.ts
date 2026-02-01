65bea44a220998d03dacd754865381d28b999734
Hyoungjoo Jin <hyoungjoo.j@gmail.com>
2026-02-01 20:06:13 +0900

    feat: migrate provider page to school specific page

---
+++ apps/web/src/features/provider/api/get-provider.ts
@@ -0,0 +1,31 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { server } from '@/lib/server/client';
+import { ProviderType, SchoolType } from '@/types/provider';
+
+type GetProviderResponse = {
+  id: string;
+  name: string;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+} & {
+  type: ProviderType;
+  schoolType: SchoolType;
+};
+
+async function getProvider(id: string) {
+  return server.get<GetProviderResponse>(`providers/${id}`).json();
+}
+
+export const GetProviderQueryOptions = (id: string) => ({
+  queryKey: ['provider', id],
+  queryFn: () => getProvider(id),
+});
+
+export default function useGetProviderQuery(id: string) {
+  return useQuery({
+    queryKey: ['provider', id],
+    queryFn: () => getProvider(id),
+  });
+}
