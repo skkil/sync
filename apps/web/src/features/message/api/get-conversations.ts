db2c88b0523303946eba75fcdbd8b8b562a65edc
Hyoungjoo Jin <hyoungjoo.j@gmail.com>
2026-02-06 09:21:05 +0900

    fix: 웹 클라이언트에서 csrf로 인한 403 error 발생 시 자동으로 retry하는 로직 구현 (#49)

--- apps/web/src/features/message/api/get-conversations.ts
+++ apps/web/src/features/message/api/get-conversations.ts
@@ -1,6 +1,6 @@
 import { UseQueryOptions, useQuery } from '@tanstack/react-query';
 
-import { server } from '@/lib/server/client';
+import { server } from '@/lib/server';
 import { Conversation } from '@/types/message';
 
 export type GetConversationsResponse = {
