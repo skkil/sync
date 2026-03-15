8777b59e4766263588ff642b7f56b8364ec49a22
jungjun0708 <jungjun0708@gmail.com>
2026-03-15 14:57:15 +0900

    style: apply formatter

--- apps/server/src/main/java/com/skkil/sync/experience/controller/ReflectionController.java
+++ apps/server/src/main/java/com/skkil/sync/experience/controller/ReflectionController.java
@@ -1,12 +1,12 @@
 package com.skkil.sync.experience.controller;
 
-import com.skkil.sync.experience.dto.request.UpdateReflectionRequest;
 import com.skkil.sync.experience.dto.request.CreateReflectionRequest;
+import com.skkil.sync.experience.dto.request.UpdateReflectionRequest;
 import com.skkil.sync.experience.dto.response.GetReflectionsResponse;
 import com.skkil.sync.experience.service.ReflectionService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -46,8 +46,7 @@ public class ReflectionController {
 
   @DeleteMapping("/experiences/{experienceId}/reflections/{reflectionId}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
-  public void deleteReflection(
-      @PathVariable Long experienceId, @PathVariable Long reflectionId) {
+  public void deleteReflection(@PathVariable Long experienceId, @PathVariable Long reflectionId) {
     reflectionService.deleteReflection(experienceId, reflectionId);
   }
 }
