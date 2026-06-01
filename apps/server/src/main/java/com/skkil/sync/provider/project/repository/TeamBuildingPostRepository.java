eed7c4e7daf9a16ae87bcfdcf87c2fa9cf3e08b2
Hyoungjoo Jin <hyoungjoo.j@gmail.com>
2026-06-01 09:39:52 +0900

    remove: legacy code for project experiences

--- apps/server/src/main/java/com/skkil/sync/provider/project/repository/TeamBuildingPostRepository.java
+++
@@ -1,6 +0,0 @@
-package com.skkil.sync.provider.project.repository;
-
-import com.skkil.sync.provider.project.model.TeamBuildingPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamBuildingPostRepository extends JpaRepository<TeamBuildingPost, Long> {}
